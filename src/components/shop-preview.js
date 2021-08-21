58acae8908f7816240806d1d51f9ebf2e572b652
carissa-tang <carissakytang@gmail.com>
2021-08-21 17:44:39 -0400

    add main colors

--- src/components/shop-preview.js
+++ src/components/shop-preview.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Container from './container'
 
-export default function AboutUs() {
+export default function ShopPreview() {
   return (
     <Container>Coming Soon!
     </Container>
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Seo from '../components/seo'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import ShopPreview from '../components/shop-preview'
 
 class BlogIndex extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location}>
         <Seo title="Shop" />
         <Hero title="Shop" />
+        <ShopPreview />
       </Layout>
     )
   }
