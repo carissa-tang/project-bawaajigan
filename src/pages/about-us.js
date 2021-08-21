cccf359a66febb3691c06cfad1bd48d86397ebfc
carissa-tang <carissakytang@gmail.com>
2021-08-21 15:15:34 -0400

    add about-us component

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Seo from '../components/seo'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import AboutUs from '../components/about-us'
 
 class BlogIndex extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location}>
         <Seo title="About Us" />
         <Hero title="About Us" />
+        <AboutUs />
       </Layout>
     )
   }
